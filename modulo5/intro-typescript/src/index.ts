64d2d0e333ca8c92c444a44c6266aaf6c7084e26
carlos martins <guto_highlander@hotmail.com>
2022-09-29 23:27:16 -0300

    intro-typescript

---
+++ modulo5/intro-typescript/src/index.ts
@@ -0,0 +1,20 @@
+/* 
+
+const a: number = 6
+const b: number = 8
+const c: number = 8
+
+function checaTriangulo(a: number, b: number, c: number) : string {
+    if (a !== b && b !== c) {
+      return "Escaleno";
+    } else if (a === b && b === c) {
+      return "Equilátero";
+    } else {
+      return "Isósceles";
+    }
+  }
+
+  console.log(checaTriangulo(a,b,c))
+  /*-----------------------------------------------------------------*/
+
+  
