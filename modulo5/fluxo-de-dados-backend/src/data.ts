bc7daa5826302979ed540c3115a9364c2e52856d
carlos martins <guto_highlander@hotmail.com>
2022-11-10 21:28:45 -0300

    fluxo-de-dados-inicio

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,27 @@
+
+// exercicio 2
+
+export type Produto =  {
+    id:string,
+    name:string,
+    price:number,
+}
+
+export const produtos: Produto[] = [
+    {
+        id: "1",
+        name:"iphone 13",
+        price: 5000,
+    },
+    {
+        id: "2",
+        name:"galaxy s22",
+        price: 6000,
+    },
+    {
+        id: "3",
+        name:"macbook",
+        price: 8000,
+    }
+]
+
